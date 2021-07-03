012a5cbb149ab4f8cd7f84d3354b610ee17f5db5
Kelvyn Santana <santanakelvyn@gmail.com>
2021-07-02 22:27:36 -0300

    fix: update next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         source: '/api/external/:path*',
-        destination: 'http://localhost:3333/:path*',
+        destination: process.env.API_URL,
       },
       {
         source: '/',
